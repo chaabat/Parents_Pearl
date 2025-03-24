7d22ca8cf1e42fb2c4f15af260d859da7184dc57
Ayoub CHAABAT <chaabat.02@gmail.com>
2025-03-24 10:49:55 +0000

    github action

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api',
-  imageBaseUrl: 'http://localhost:8080/api/uploads/images',
+  apiUrl: 'http://localhost:8081/api',
+  imageBaseUrl: 'http://localhost:8081/api/uploads/images',
+  databaseUrl: 'postgresql://localhost:5432/parentpearl',
 };
